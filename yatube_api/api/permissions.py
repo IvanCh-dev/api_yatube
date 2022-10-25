4d3e0e748d2aa1fd13fe52122b9bf4ac9a616d48
Ivan Chuzhba <ivan94-20@yandex.ru>
2022-10-25 23:14:05 +0600

    Fixed_5

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,6 +2,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    """Проверка, является ли пользователь автором или админом."""
+    """Проверка, является ли пользователь автором или у него будет доступ
+    только для чтения.
+    """
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
